d0b803ea07af06cd1eaa10ae4977c25c22f8ccb7
Maks Čuka <maks.cuka@student.um.si>
2026-04-22 13:49:58 +0200

    zbirsal user_model.py ker je nepotreben

--- src/models/user_model.py
+++
@@ -1,7 +0,0 @@
-from db import db
-class User(db.Model):
-    __tablename__ = "users"
-
-    id = db.Column(db.Integer, primary_key=True)
-    username = db.Column(db.String(100), unique=True, nullable=False)
-    password = db.Column(db.String(200), nullable=False)
