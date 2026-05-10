d6fc037f31f3f9550b532c2026dd0e6ead580387
NikiKolerič <niki.koleric@gmail.com>
2026-05-10 19:39:53 +0200

    dodajanje 10

--- src/app.py
+++ src/app.py
@@ -20,6 +20,7 @@ import controllers.rezervacije_stranke_controller
 import controllers.frizer_controller
 import controllers.blokade_controller
 import controllers.moje_rezervacije_controller
+import controllers.preklic_rezervacije_controller
 
 f_app = Flask(__name__, template_folder='templates')
 f_app.secret_key = os.environ.get('SECRET_KEY', 'pls spremeni')
@@ -197,8 +198,12 @@ def admin():
 @f_app.route("/frizer")
 @frizer_required
 def frizer():
-    return controllers.sv_setup.frizer()
+    return controllers.preklic_rezervacije_controller.frizer_panel()
 
+@f_app.route("/frizer/preklic/<int:id_rezervacije>", methods=["POST"])
+@frizer_required
+def frizer_preklic_rezervacije(id_rezervacije):
+    return controllers.preklic_rezervacije_controller.preklic_rezervacije(id_rezervacije)
 @f_app.route("/stranka")
 @login_required
 def stranka():
