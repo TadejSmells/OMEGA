e044528dbe9784535a484248f9f56e04e7bac117
JernejD <114926812+JernejDob@users.noreply.github.com>
2026-05-18 00:00:37 +0200

    updatano, user story 80
    dodana backend logika in nova stran, poleg tega se sedaj izpise popup na zacetni strani ko je strani skencelana rezervacija

--- src/controllers/index.py
+++ src/controllers/index.py
@@ -1,5 +1,7 @@
-from flask import render_template
+from flask import render_template, session
 from models import model_salon
+from models import model_rezervacije_stranke
+
 
 def home():
     try:
@@ -8,4 +10,20 @@ def home():
     except Exception:
         saloni = []
         storitve = []
-    return render_template("zacetna_stran.html", saloni=saloni, storitve=storitve)
+
+    preklicane_rezervacije = []
+
+    if session.get("user_id") and session.get("role") == "stranka":
+        try:
+            preklicane_rezervacije = model_rezervacije_stranke.get_preklicane_rezervacije_uporabnika(
+                session["user_id"]
+            )
+        except Exception:
+            preklicane_rezervacije = []
+
+    return render_template(
+        "zacetna_stran.html",
+        saloni=saloni,
+        storitve=storitve,
+        preklicane_rezervacije=preklicane_rezervacije
+    )
